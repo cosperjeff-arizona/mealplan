79a6eb8794577f6a3636d69148d947fee9dfe9c3
cosperjeff-arizona <cosper.jeff@gmail.com>
2025-10-13 01:01:40 -0700

    Update plan-manifest.js

--- plan-manifest.js
+++ plan-manifest.js
@@ -2,11 +2,12 @@
 
 const allPlans = [
   {
-    id: "2025-10-06",
-    name: "Week of October 6, 2025"
-  },
-  {
     id: "2025-10-12",
     name: "Week of October 12, 2025"
+  },
+  {
+    id: "2025-10-19",
+    name: "Week of October 19, 2025"
   }
+
 ];
